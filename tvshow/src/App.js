e4f6ef6ef2ad41174dcf25365ea5a95d0dfa9c7a
JoeOs <josedoomaor+github@gmail.com>
2021-01-29 17:32:48 -0800

    First step done

---
+++ tvshow/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav><ul><li>Home</li><li>About</li><li>Episodes</li></ul></nav>
+        <p>
+          Seinfeld
+        </p>
+        <p>The contuining misadventurs of neurotic New York City stand-up comedian</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
