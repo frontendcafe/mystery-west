1043a2841f7673dfe9fcfad3354624b9b2002a01
JoeOs <josedoomaor+github@gmail.com>
2021-02-11 19:54:27 -0800

    routes: about, episodes, home

--- tvshow/src/App.js
+++ tvshow/src/App.js
@@ -1,17 +1,44 @@
+import React from "react";
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
         <nav><ul><li>Home</li><li>About</li><li>Episodes</li></ul></nav>
-        <p>
-          Seinfeld
-        </p>
-        <p>The contuining misadventurs of neurotic New York City stand-up comedian</p>
+        <Switch>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/episodes'>
+            <Episodes />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
       </header>
     </div>
+    </Router>
   );
 }
 
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>
+}
+
+function Episodes() {
+  return <h2>Episodes</h2>
+}
 export default App;
