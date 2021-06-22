31fe787be934e16dbba4507d3475c3f4c6f5d3e0
Nar <51009725+narvmtz@users.noreply.github.com>
2021-06-21 23:30:53 -0500

    Merged short list component to dataShow

--- tvshow/src/components/dataShow.js
+++ tvshow/src/components/dataShow.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { useFetch } from './useFetch';
-import ShortList from './shortList';
+import { Link } from 'react-router-dom';
+import { useQuery } from 'react-query';
+
 import imageShow from '../images/seinfeld.png';
 import '../styles/datashow.css';
 
 const DataShow = () => {
   const url = 'http://api.tvmaze.com/shows/530';
 
-  const { data, hasError } = useFetch(url);
+  const { data: dataShow, isError: hasError } = useQuery(
+    'show',
+    () => fetch(`${url}`).then((res) => res.json()),
+    { refetchOnWindowFocus: false }
+  );
 
   return (
     <React.Fragment>
@@ -15,19 +20,62 @@ const DataShow = () => {
         <div>An error has occured.</div>
       ) : (
         <React.Fragment>
-          <h2>{data?.name}</h2>
           <section className="showDescription">
             <img src={imageShow} alt="show poster" className="showImage" />
             <div className="textDescription">
               <div
                 className="description"
-                dangerouslySetInnerHTML={{ __html: data?.summary }}
+                dangerouslySetInnerHTML={{ __html: dataShow?.summary }}
               ></div>
               <ShortList />
             </div>
           </section>
         </React.Fragment>
       )}
+    </React.Fragment>
+  );
+};
+
+const ShortList = () => {
+  const URL = 'http://api.tvmaze.com/shows/530/episodes';
+
+  const { data: episodeData, isError: hasError } = useQuery(
+    'episodeList',
+    () => fetch(`${URL}`).then((res) => res.json()),
+    { refetchOnWindowFocus: false }
+  );
+
+  return (
+    <React.Fragment>
+      <h4>Episode List</h4>
+      <ul className="episodeList">
+        <div className="summaryList">
+          <li className="episodeNum title">Season</li>
+          <li className="episodeNum title">Episode</li>
+          <li className="episodeName title">Name</li>
+        </div>
+        {hasError ? (
+          <div>An error has occured.</div>
+        ) : (
+          episodeData
+            ?.filter(
+              (episodes) => episodes.season === 1 && episodes.number <= 5
+            )
+            .map((item) => (
+              <div key={item.id} className="summaryList">
+                <li className="episodeNum number">{item.season} </li>
+                <li className="episodeNum number">{item.number} </li>
+                <Link
+                  className="episodeName"
+                  to={`/id=${item.id}/season_${item.season}/episode_${item.number}`}
+                >
+                  {item.name}
+                </Link>
+              </div>
+            ))
+        )}
+      </ul>
+      <Link to="/episodes">Full episode list</Link>
     </React.Fragment>
   );
 };
